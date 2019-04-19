c8dcc042492c23f8e1c4865291ffb097df7f5177
Philipp Gabler <phipsgabler@users.noreply.github.com>
2019-04-19 11:05:09 +0200

    Simplify basic tracking using overdub

--- src/cassette.jl
+++ src/cassette.jl
@@ -3,45 +3,35 @@ using Cassette
 Cassette.@context TrackingCtx
 
 mutable struct Tracker
-    isfirst::Bool
-    current::Union{Nothing, Call}
-    stack::Vector{Call}
-    Tracker() = new(true, nothing, Call[])
+    root::Union{Nothing, Call}
+    parent::Union{Nothing, Call}
 end
 
-function enter!(t::Tracker, f, args)
+Tracker() = Tracker(nothing, nothing)
+
+
+function Cassette.overdub(ctx::TrackingCtx, f, args...)
+    tracker = ctx.metadata
     call = Call(f, args)
-    if !t.isfirst
-        push!(t.current.body, call)
-        push!(t.stack, t.current)
-        t.current = call
+    
+    oldparent = tracker.parent
+
+    if oldparent === nothing # this was the first call
+        tracker.root = call
     else
-        t.current = call
-        push!(t.stack, t.current)
-        t.isfirst = false
+        push!(oldparent.body, call)
     end
-    return nothing
+
+    tracker.parent = call
+    result = Cassette.recurse(ctx, f, args...)
+    tracker.parent = oldparent
+    
+    return result
 end
 
-function exit!(t::Tracker, output, f, args)
-    t.current = pop!(t.stack)
-    return nothing
-end
-
-Cassette.prehook(ctx::TrackingCtx, f, args...) = enter!(ctx.metadata, f, args)
-Cassette.posthook(ctx::TrackingCtx, output, f, args...) = exit!(ctx.metadata, output, f, args)
-
-# function Cassette.overdub(ctx::TrackingCtx, first::Val{true}, f, args...)
-#     if Cassette.canrecurse(ctx, f, args...)
-#         _ctx = Cassette.similarcontext(ctx, metadata = callback)
-#         return Cassette.recurse(_ctx, f, args...) # return result, callback
-#     else
-#         return Cassette.fallback(ctx, f, args...), callback
-#     end
-# end
 
 function track(f, args...)
-    ctx = TrackingCtx(metadata = Tracker())
-    r = Cassette.overdub(ctx, () -> f(args...))
-    r, ctx.metadata.current
+    ctx = Cassette.disablehooks(TrackingCtx(metadata = Tracker()))
+    r = Cassette.@overdub ctx f(args...)
+    r, ctx.metadata.root
 end
