896bcc5ee656fad6c23ee9635fe73013a522ed13
Philipp Gabler <phipsgabler@users.noreply.github.com>
2019-08-07 10:33:11 +0200

    Update project basics

--- src/DynamicComputationGraphs.jl
+++ src/DynamicComputationGraphs.jl
@@ -1,7 +1,8 @@
 module DynamicComputationGraphs
 
+# include("tracing.jl")
 include("graph.jl")
-include("cassette.jl")
+# include("cassette.jl")
 # include("tracker.jl")
 # include("idset.jl")
 # include("params.jl")
