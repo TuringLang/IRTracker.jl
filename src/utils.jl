4087a851bd2af0d51be415095fac0dd755f65302
Philipp Gabler <phipsgabler@users.noreply.github.com>
2019-08-23 12:14:03 +0200

    Delete traces of experiment

--- src/utils.jl
+++ src/utils.jl
@@ -39,31 +39,5 @@ struct TapeIndex
     id::Int
 end
 
-
-const VarOrNewVar = Union{IRTools.Variable, IRTools.NewVariable}
-
-struct VarToRecordDict
-    old_variables::Dict{IRTools.Variable, TapeIndex}
-    new_variables::Dict{VarOrNewVar, IRTools.Variable}
-end
-
-VarToRecordDict() = VarToRecordDict(Dict{IRTools.Variable, TapeIndex}(),
-                                    Dict{VarOrNewVar, IRTools.Variable}())
-
-record_old_variable!(d::VarToRecordDict, v::IRTools.Variable) =
-    (push!(d.old_variables, v => TapeIndex(length(d.old_variables) + 1)); d)
-record_new_variable!(d::VarToRecordDict, new_var::VarOrNewVar, old_var::IRTools.Variable) =
-    (push!(d.new_variables, new_var => old_var); d)
-
-
 reify_quote(expr) = Expr(:copyast, QuoteNode(expr))
 
-translate_old_variable(d::VarToRecordDict, var::IRTools.Variable) = d.old_variables[var]
-translate_old_variable(d::VarToRecordDict, expr::Expr) =
-    Expr(expr.head, map(expr -> translate_old_variable(d, expr), expr.args)...)
-translate_old_variable(::VarToRecordDict, expr) = expr
-
-translate_new_variable(d::VarToRecordDict, var::VarOrNewVar) = d.new_variables[var]
-translate_new_variable(d::VarToRecordDict, expr::Expr) =
-    Expr(expr.head, map(expr -> translate_new_variable(d, expr), expr.args)...)
-translate_new_variable(::VarToRecordDict, expr) = expr
