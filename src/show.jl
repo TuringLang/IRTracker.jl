5736290541387eb20964c1f7d42ec6c476492a54
Philipp Gabler <phipsgabler@users.noreply.github.com>
2019-10-02 15:04:41 +0200

    Switch show implementations to pass maxlevel in IOContext

--- src/show.jl
+++ src/show.jl
@@ -1,57 +1,61 @@
 import Base: show
 
 
-printlevels(tape::GraphTape, levels) = show(stdout, tape, maxlevel = levels - 1)
+printlevels(tape::GraphTape, levels) = show(IOContext(stdout, :maxlevel => levels - 1), tape)
+printlevels(node::Node, levels) = show(IOContext(stdout, :maxlevel => levels - 1), node)
 
 printvalue(io, value) = print(IOContext(io, :limit => true), value)
 printvalue(io, n::Nothing) = print(io, repr(n))
 
-function show(io::IO, tape::GraphTape, level = 0; maxlevel = typemax(level))
+function show(io::IO, tape::GraphTape, level = 0)
+    maxlevel = get(io, :maxlevel, typemax(level))
     level > maxlevel && return
+    
     for (i, node) in enumerate(tape.nodes)
         print(io, " " ^ 2level, "@", i, ": ")
-        show(io, node, level; maxlevel = maxlevel)
+        show(io, node, level)
         i < length(tape.nodes) && print(io, "\n")
     end
 end
 
-function show(io::IO, node::Constant, level = 0; maxlevel = typemax(level))
+function show(io::IO, node::Constant, level = 0)
     print(io, "[Constant ", node.index, "] = ")
     printvalue(io, node.value)
 end
 
-function show(io::IO, node::PrimitiveCall, level = 0; maxlevel = typemax(level))
+function show(io::IO, node::PrimitiveCall, level = 0)
     print(io, "[", node.index, "] ")
     print(io, node.expr, " = ")
     printvalue(io, node.value)
 end
 
-function show(io::IO, node::NestedCall, level = 0; maxlevel = typemax(level))
+function show(io::IO, node::NestedCall, level = 0)
+    maxlevel = get(io, :maxlevel, typemax(level))
     print(io, "[", node.index, "] ")
     print(io, node.expr, " = ")
     printvalue(io, node.value)
     level < maxlevel && print(io, "\n") # prevent double newlines in limited printing
-    show(io, node.subtape, level + 1; maxlevel = maxlevel)
+    show(io, node.subtape, level + 1)
 end
 
-function show(io::IO, node::SpecialStatement, level = 0; maxlevel = typemax(level))
+function show(io::IO, node::SpecialStatement, level = 0)
     print(io, "[", node.index, "] ")
     print(io, node.expr, " = ")
     printvalue(io, node.value)
 end
 
-function show(io::IO, node::Argument, level = 0; maxlevel = typemax(level))
+function show(io::IO, node::Argument, level = 0)
     print(io, "[Argument ", node.index, "] = ")
     printvalue(io, node.value)
 end
 
-function show(io::IO, node::Return, level = 0; maxlevel = typemax(level))
+function show(io::IO, node::Return, level = 0)
     print(io, "[", node.index, "] ")
     print(io, "return ", node.expr, " = ")
     printvalue(io, node.value)
 end
 
-function show(io::IO, node::Branch, level = 0; maxlevel = typemax(level))
+function show(io::IO, node::Branch, level = 0)
     print(io, "[", node.index, "] ")
     print(io, "goto §", node.target)
     L = length(node.arg_exprs) 
