e93e3d5482993a29ba7ef985eeca3d15e8123135
Philipp Gabler <phipsgabler@users.noreply.github.com>
2019-10-18 11:06:04 +0200

    Add test5 for long-range conjugacy model

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,52 +1,69 @@
 using Test
 using DynamicComputationGraphs
 using IRTools: @code_ir
+using Distributions
 
 
 ########### Basic sanity checks #################
-f(x) = x + 1
-track(f, 42)
+# f(x) = x + 1
+# track(f, 42)
 
-weird(n) = rand() < 1/(n + 1) ? n : weird(n + 1)
-track(weird, 3)
+# weird(n) = rand() < 1/(n + 1) ? n : weird(n + 1)
+# track(weird, 3)
 
-geom(n, β) = rand() < β ? n : geom(n + 1, β)
-track(geom, 3, 0.5)
+# geom(n, β) = rand() < β ? n : geom(n + 1, β)
+# track(geom, 3, 0.5)
 
-function test1(x)
-    t = (x, x)
-    t[1] + 1
-end
-track(test1, 42)
+# function test1(x)
+#     t = (x, x)
+#     t[1] + 1
+# end
+# track(test1, 42)
 
-function test2(x)
-    if x < 0
-        return x + 1
+# function test2(x)
+#     if x < 0
+#         return x + 1
+#     else
+#         return x - 1 #sum([x, x])
+#     end
+# end
+# track(test2, 42)
+
+# function test3(x)
+#     y = 0 #zero(x)
+#     while x > 0
+#         y += 1
+#         x -= 1
+#     end
+
+#     return y
+# end
+# track(test3, 42)
+
+# test4(x) = [x, x]
+# track(test4, 42)
+
+
+function test5()
+    p = rand(Beta(1, 1))
+    conj = rand(Bernoulli(p))
+    if conj
+        m = rand(Normal(0, 1))
     else
-        return x - 1 #sum([x, x])
-    end
-end
-track(test2, 42)
-
-function test3(x)
-    y = 0 #zero(x)
-    while x > 0
-        y += 1
-        x -= 1
+        m = rand(Gamma(3, 2))
     end
 
-    return y
+    m += 2
+    return Normal(m, 1)
 end
-track(test3, 42)
+track(test5)
 
-test4(x) = [x, x]
-track(test4, 42)
 
-# check visible result
-result, graph = track(geom, 3, 0.6)
-@show @code_ir geom(3, 0.6)
-printlevels(graph, 2)
-println("\n")
+# # check visible result
+# result, graph = track(geom, 3, 0.6)
+# @show @code_ir geom(3, 0.6)
+# printlevels(graph, 2)
+# println("\n")
 
 # @show @code_ir test2(0.3)
 # println(@code_ir test2(3))
