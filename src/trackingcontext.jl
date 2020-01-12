147b800e2ca116daec4ca9c35dae13d99a1d7f62
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-01-12 20:39:38 +0100

    Add draft for context composition

--- src/trackingcontext.jl
+++ src/trackingcontext.jl
@@ -24,3 +24,56 @@ function trackednested(ctx::DepthLimitContext, f_repr::TapeExpr,
     new_ctx = increase_level(ctx)
     return recordnestedcall(new_ctx, f_repr, args_repr, info)
 end
+
+
+
+"""Composes behaviour of a series of other contexts (right to left, as with functions)."""
+struct ComposedContext{T<:Tuple{Vararg{AbstractTrackingContext}}} <: AbstractTrackingContext
+    contexts::T
+    ComposedContext(contexts::T) where {T<:Tuple{Vararg{AbstractTrackingContext}}} =
+        new{T}(contexts)
+end
+
+ComposedContext(ctx::AbstractTrackingContext, contexts::AbstractTrackingContext...) =
+    ComposedContext((ctx, contexts...))
+
+
+function foldcontexts(composed::ComposedContext, track, args...)
+    contexts = composed.contexts
+    C = length(contexts)
+    node = track(contexts[end], args...)
+    
+    for c = C:-1:2
+        node = track(composed.context[c], args...)
+    end
+
+    return node
+end
+
+
+trackedreturn(composed::ComposedContext, arg_repr::TapeExpr, info::NodeInfo) =
+    foldcontexts(composed, arg_repr, info)
+
+trackedjump(composed::ComposedContext, target::Int, args_repr::ArgumentTuple{TapeValue},
+            cond_repr::TapeExpr, info::NodeInfo) =
+                foldcontexts(composed, target, args_repr, cond_repr, info)
+
+trackedspecial(composed::ComposedContext, form_repr::TapeExpr, info::NodeInfo) =
+    foldcontexts(composed, form_repr, info)
+
+trackedconstant(composed::ComposedContext, const_repr::TapeExpr, info::NodeInfo) =
+    foldcontexts(composed, const_repr, info)
+
+trackedargument(composed::ComposedContext, arg_repr::TapeExpr, number::Int, info::NodeInfo) =
+    foldcontexts(composed, arg_repr, number, info)
+
+trackedprimitive(trackedprimitive(composed::AbstractTrackingContext, f_repr::TapeExpr,
+                                  args_repr::ArgumentTuple{TapeExpr}, info::NodeInfo)) =
+                                      foldcontexts(composed, f_repr, args_repr, info)
+
+trackednested(trackedprimitive(composed::AbstractTrackingContext, f_repr::TapeExpr,
+                               args_repr::ArgumentTuple{TapeExpr}, info::NodeInfo)) =
+                                   foldcontexts(composed, f_repr, args_repr, info)
+
+canrecur(composed::ComposedContext, f, args...) =
+    any(canrecur(ctx, f, args...) for ctx in composed.contexts)
