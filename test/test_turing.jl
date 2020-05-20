ff34f59c76dedfb43be071755b2731972ebde931
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-05-20 15:10:20 +0200

    Add test for AutoGibbs context

--- test/test_turing.jl
+++ test/test_turing.jl
@@ -1,5 +1,6 @@
-using Turing
 using Distributions
+using DynamicPPL
+using Turing
 
 
 const observations = randn(10) .+ range(1, step = 0.3, length = 10)
@@ -41,15 +42,35 @@ end
 end
 
 
+# See AutoGibbs
+const ModelEval = Union{typeof(DynamicPPL.evaluate_singlethreaded),
+                  typeof(DynamicPPL.evaluate_multithreaded)}
+
+
+struct AutoGibbsContext{F} <: AbstractTrackingContext end
+
+IRTracker.canrecur(ctx::AutoGibbsContext{F}, ::Model{F}, args...) where {F} = true
+IRTracker.canrecur(ctx::AutoGibbsContext{F}, ::ModelEval, ::Model{F}, args...) where {F} = true
+IRTracker.canrecur(ctx::AutoGibbsContext{F}, f::F, args...) where {F} = true
+IRTracker.canrecur(ctx::AutoGibbsContext, f, args...) = false
+
+trackmodel(model::Model{F}) where {F} = track(AutoGibbsContext{F}(), model)
+
+
 @testset "Turing" begin
     # with depth 7 this fails in the Dict's _setindex! method due to unitialized values
+    # [1] getindex at ./array.jl:744 [inlined]
+    # [2] _iterate at ./dict.jl:675 [inlined]
+    # [3] iterate at ./dict.jl:677 [inlined]
+    # [4] deepcopy_internal(::Dict{String,BitArray{1}}, ::IdDict{Any,Any}) at ./deepcopy.jl:112
+    # [5] deepcopy at ./deepcopy.jl:30 [inlined]
+
     @test track(DepthLimitContext(6), KalmanFilter(observations)) isa NestedCallNode
     @test track(DepthLimitContext(6), MutatingKalmanFilter(observations)) isa NestedCallNode
+    
+    @test trackmodel(KalmanFilter(observations)) isa NestedCallNode
+    @test trackmodel(MutatingKalmanFilter(observations)) isa NestedCallNode
 end
 
-   # [1] getindex at ./array.jl:744 [inlined]
-   # [2] _iterate at ./dict.jl:675 [inlined]
-   # [3] iterate at ./dict.jl:677 [inlined]
-   # [4] deepcopy_internal(::Dict{String,BitArray{1}}, ::IdDict{Any,Any}) at ./deepcopy.jl:112
-   # [5] deepcopy at ./deepcopy.jl:30 [inlined]
+
 
